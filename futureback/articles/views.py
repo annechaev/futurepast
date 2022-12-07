02c2f2d573bad1cd468c9961994f0d7921cea9c3
romantiqueamour <romantiqueamour03@yandex.ru>
2022-12-07 17:42:57 +0300

    added docker-compose

--- futureback/articles/views.py
+++ futureback/articles/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import View
 
 
 # Create your views here.
-class ArticlesHome(TemplateView):
+class ArticlesHome(View):
 
     template_name = 'articles/index.html'
+
+    def get(self, request):
+        return render(request, self.template_name)
